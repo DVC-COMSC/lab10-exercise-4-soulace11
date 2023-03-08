7bc1a74bfda2b23d9b949824b7fe2886af4d5a42
soulace11 <126546042+soulace11@users.noreply.github.com>
2023-03-08 22:02:27 +0000

    Complete E4

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+  celcius = int(input('Enter the Celcius Degree:')) 
+  farenheit = (9.0 / 5.0) * celcius + 32
+  print(f'The Fareneheit is {farenheit:.2f}')
+  pass
 
 
 if __name__ == '__main__':
